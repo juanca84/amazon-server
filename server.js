4358548ec0ba4c436024fff986cb6f4787041b28
Juan Carlos Condori <juanc.com@gmail.com>
2020-07-01 23:13:56 -0400

    Adición de conexión con una base de datos mongoDB en la nube

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+dotenv.config();
+ 
 const app = express()
+
+mongoose.connect(
+    process.env.DATABASE,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    err => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("connected to the database");
+        }
+    }
+);
 
 // Middlewares
 app.use(morgan('dev'));
