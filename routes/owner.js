19b375739f4ec412828dc045a5618f22a46c9728
Juan Carlos Condori Machicado <jcondori@agetic.gob.bo>
2020-07-30 11:38:36 -0400

    Adición de servicios para owners

---
+++ routes/owner.js
@@ -0,0 +1,39 @@
+const router = require("express").Router();
+const Owner = require("../models/owner");
+
+// POST api
+router.post('/owners', async(req, res) => {
+    try {
+        let owner = new Owner();
+        owner.name = req.body.name;
+        owner.about = req.body.about;
+        await owner.save();
+        res.json({
+            success: true,
+            message: "Successfuly createda a new owner"
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        })
+    }
+})
+
+// GET api
+router.get('/owners', async(req, res) => {
+    try {
+        let owners = await Owner.find();
+        res.json({
+            success: true,
+            owners: owners
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        })
+    }
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -33,8 +33,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // require apis
-const productRoutes = require('./routes/product')
+const productRoutes = require('./routes/product');
+const categoryRoutes = require('./routes/category');
+const ownerRoutes = require('./routes/owner');
+
 app.use('/api', productRoutes);
+app.use('/api', categoryRoutes);
+app.use('/api', ownerRoutes);
 
 app.listen(3000, err => {
     if (err) {
