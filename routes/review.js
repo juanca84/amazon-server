8c188863b9b11fecd194ddafcf852df382b76f4b
Juan Carlos Condori Machicado <juanc.com@gmail.com>
2020-08-20 23:23:37 -0400

    corrigiendo algunos errores para almacenar los reviews

--- routes/review.js
+++ routes/review.js
@@ -7,14 +7,15 @@ const upload = require("../middlewares/upload-photo");
 router.post("/reviews/:productID", [verifyToken, upload.single("photo")], async (req, res) => {
     try {
         const review = new Review();
+        console.log(req.body);
         review.headline = req.body.headline; 
         review.body = req.body.body;
         review.rating = req.body.rating;
         review.photo = req.file.location;
-        review.user = req.decoded._id;
+        review.userID = req.decoded._id;
         review.productID = req.params.productID;
 
-        await Product.update({ $push: review._id });
+        await Product.update({ $push: { rating: review._id } });
         const savedReview = await review.save();
         if (savedReview) {
             res.json({
