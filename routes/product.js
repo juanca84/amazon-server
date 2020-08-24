055244d3ebeafdeba0ff9f9a22eafa7cb7180a6c
Juan Carlos Condori Machicado <juanc.com@gmail.com>
2020-08-23 22:17:17 -0400

    modificaciones en los reviews

--- routes/product.js
+++ routes/product.js
@@ -32,7 +32,10 @@ router.post("/products", upload.single("photo"), async (req, res) => {
 // GET request - get all products
 router.get('/products', async(req, res) => {
     try {
-        let products = await Product.find().populate('owner category').exec();
+        let products = await Product.find()
+            .populate('owner category')
+            .populate('reviews', 'rating')
+            .exec();
         res.json({
             success: true,
             products: products
@@ -48,7 +51,10 @@ router.get('/products', async(req, res) => {
 // GET request - create  a single product
 router.get('/products/:id', async(req, res) => {
     try {
-        let product = await Product.findOne({ _id: req.params.id }).populate('owner category').exec();
+        let product = await Product.findOne({ _id: req.params.id })
+            .populate('owner category')
+            .populate('reviews', 'rating')
+            .exec();
         res.json({
             success: true,
             product: product
